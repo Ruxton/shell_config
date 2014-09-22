0199a807610e1a8cf848160d0e95a7a3cef07287
Ruxton <ruxton@thefrontiergroup.com.au>
2014-09-22 14:01:20 +0800

    Remove test flight uploader add restartadb

--- .profile.d/android.bash
+++ .profile.d/android.bash
@@ -92,71 +92,10 @@ compile: Compile android project with maven (mvn clean install)
   fi
 }
 
-# testflightup: Upload APK files from this
-function testflightup() {
-
-  # contains TESTFLIGHT_API_TOKEN & TESTFLIGHT_TEAM_TOKEN & TESTFLIGHT_DISTRIBUTION_LISTS
-  source ~/.testflight
-
-  echo
-  echo "TestFlightApp Uploader"
-  echo
-
-  local notes current_date default_notes
-  local notify=False
-  local distribution_lists=$TESTFLIGHT_DISTRIBUTION_LISTS
-
-  while [ "${1+defined}" ]; do
-    case "$1" in
-      --help)         cat <<-EOF
-Usage: testflightup [options]
-
-OPTIONS:
-    --help                  See this message
-    --notify                Notify users in the default distribution list of the new version
-    --note <notes>          Set the note to be set for the build
-    --distribution <list>   A comma seperated list of valid distribution lists ie. "Android,Android The Place"
-EOF
-                      return 0
-                      ;;
-      --distribution) shift
-                      distribution_lists=$1
-                      ;;
-      --notify)       notify=True
-                      ;;
-      --note)         shift
-                      local inNotes=$1
-                      ;;
-    esac
-    shift
-  done
-
-
-  __apk_selector
-
-
-  if [[ "$selected_apk" == "" ]]; then
-    echo "No APK file selected, exiting."
-    return 0
-  fi
-
-  current_date=`date`
-  default_notes="Deployed $current_date"
-
-  if [ "${inNotes+defined}" ]; then
-    default_notes=$inNotes
-  fi
-
-  read -ep "Deploy notes (default $default_notes): " notes;
-  notes=${notes:-$default_notes}
-
-  curl https://testflightapp.com/api/builds.json \
-    -F file=@target/$selected_apk \
-    -F api_token=$TESTFLIGHT_API_TOKEN \
-    -F team_token=$TESTFLIGHT_TEAM_TOKEN \
-    -F notes="$notes" \
-    -F notify=$notify \
-    -F distribution_lists="$distribution_lists"
+# adbrestart: restart Android Debug Bridge
+function adbrestart() {
+  $ANDROID_HOME/platform-tools/adb kill-server
+  $ANDROID_HOME/platform-tools/adb start-server
 }
 
 function __apk_selector() {
